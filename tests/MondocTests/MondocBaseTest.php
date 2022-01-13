8c5938e9211d2501ff44eef1ce94ad720b7f93e9
Roger Thomas <rogere84@gmail.com>
2022-01-13 12:52:13 +0000

    Connection

---
+++ tests/MondocTests/MondocBaseTest.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * District5 Mondoc Library
+ *
+ * @author      District5 <hello@district5.co.uk>
+ * @copyright   District5 <hello@district5.co.uk>
+ * @link        https://www.district5.co.uk
+ *
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace District5Tests\MondocTests;
+
+use District5\Mondoc\MondocConfig;
+use District5Tests\MondocTests\Example\DateModel;
+use District5Tests\MondocTests\Example\DateService;
+use District5Tests\MondocTests\Example\MyModel;
+use District5Tests\MondocTests\Example\MyService;
+use District5Tests\MondocTests\Example\MySubService;
+use District5Tests\MondocTests\Example\Subs\MyModelWithSub;
+use MongoDB\Client;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class MondocBaseTest.
+ *
+ * @package District5Tests\MondocTests
+ */
+abstract class MondocBaseTest extends TestCase
+{
+    /**
+     * @var null|string
+     */
+    protected $uniqueKey;
+
+    /**
+     * @var null|MondocConfig
+     */
+    protected $mondoc;
+
+    /**
+     * @return null|string
+     */
+    public function getUniqueKey(): ?string
+    {
+        if (null === $this->uniqueKey) {
+            $this->uniqueKey = uniqid().microtime(false);
+        }
+
+        return $this->uniqueKey;
+    }
+
+    protected function initMongo()
+    {
+        $connectionString = 'mongodb+srv://';
+        throw new \Exception('Please add the connection above!');
+
+        $connection = new Client($connectionString);
+        $this->mondoc = MondocConfig::getInstance();
+        $this->mondoc->addDatabase(
+            $connection->selectDatabase('tests'),
+            'default'
+        );
+        $this->mondoc->addServiceMapping(
+            DateModel::class,
+            DateService::class
+        )->addServiceMapping(
+            MyModel::class,
+            MyService::class
+        )->addServiceMapping(
+            MyModelWithSub::class,
+            MySubService::class
+        );
+
+        return $this->mondoc->getDatabase();
+    }
+
+    protected function tearDown()
+    {
+        $this->initMongo();
+        $this->mondoc->getDatabase()->dropCollection('test_model');
+        $this->mondoc->getDatabase()->dropCollection('date_model');
+    }
+}

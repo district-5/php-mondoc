bd090e6378a5b3c2e73d43cbe979e1db9007c79e
Roger Thomas <rogere84@gmail.com>
2022-04-02 14:48:33 +0100

    usort

--- src/Mondoc/Traits/Aggregation/FinancialCandlesTrait.php
+++ src/Mondoc/Traits/Aggregation/FinancialCandlesTrait.php
@@ -148,18 +148,18 @@ trait FinancialCandlesTrait
                     /* @var $first AggregateFinancialCandleDto */
                     /* @var $second AggregateFinancialCandleDto */
                     if ($first->getDate() < $second->getDate()) {
-                        return -1;
+                        return 1;
                     }
-                    return 1;
+                    return -1;
                 });
             } else {
                 usort($candles, function ($first, $second) {
                     /* @var $first AggregateFinancialCandleDto */
                     /* @var $second AggregateFinancialCandleDto */
                     if ($first->getDate() < $second->getDate()) {
-                        return 1;
+                        return -1;
                     }
-                    return -1;
+                    return 1;
                 });
             }
         }
